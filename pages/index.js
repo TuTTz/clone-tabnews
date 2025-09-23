6d1210ca12c4d3e1424adfb5707a294c8a878932
TuTTzGP <arthurpirino@gmail.com>
2025-09-23 00:30:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Pois é mais uma coisinha para ver se funciona Teste DEPLO</h1>
+function Home() {
+  return <h1>Pois é mais uma coisinha para ver se funciona Teste DEPLO</h1>;
 }
 
 export default Home;
